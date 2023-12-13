d172bb123ff7d8246f5e42fdb01830c41afadd24
BonGiorn0 <147716333+BonGiorn0@users.noreply.github.com>
2023-12-13 14:13:56 +0200

    Add files via upload
    Server addded

---
+++ Server.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <vector>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define DEFAULT_PORT 1488
+#define BUFFER_SIZE 1024
+
+class Server {
+public:
+    Server();
+    ~Server();
+    void start();
+    void stop();
+
+private:
+    SOCKET serverSocket;
+    std::vector<std::thread> clientThreads;
+
+    void clientHandler(SOCKET clientSocket);
+};
